3fa70ecbfbf0c5681dfb8afaf3a8040909175010
jacob8ng <jacob8ng@yahoo.com>
2015-08-21 08:37:09 -0700

    Pokemon
    Pikachu!

---
+++ Pokemon/Pikachu.java
@@ -0,0 +1,13 @@
+/*
+ * Jacob Ng
+ * APCS
+ * Pikachu
+ */
+public class Pikachu{
+  public static void main(String[] args){
+    System.out.println("Pikachu welcomes you to the world of Pokemon!");
+    System.out.println("\t  (\\__/)");
+    System.out.println("\t  (o^.^)");
+    System.out.println("\t z(_\")(\")");
+  }
+}
